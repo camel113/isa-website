731a5c76003efaac73e65beb6e72c3b659884c61
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2013-11-21 20:24:35 +0100

    Change exposistion title style

--- expositions.php
+++ expositions.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <?php include('head.php')?>
+    <?php include('head.php') ?>
 </head>
 <body>
 <div id="page">
 <h1>Isabelle Pilloud</h1>
 <div id="menu">
-    <?php include('menu.php')?>
+    <?php include('menu.php') ?>
 </div>
 <div id="content">
     <h1 class="ChoixExpo" id="expoSelect"><a href="expositions.php">Expositions</a></h1>
