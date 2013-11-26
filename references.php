51d364eba7a84221f6ce9e4ff6389c71cc2c9c22
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2013-11-26 17:06:56 +0100

    Add gitignore

--- references.php
+++ references.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <?php include('head.php')?>
+    <?php include('head.php') ?>
 </head>
 <body>
 <div id="page">
 <h1>Isabelle Pilloud</h1>
 <div id="menu">
-    <?php include('menu.php')?>
+    <?php include('menu.php') ?>
 </div>
 <div id="content">
     <h1 id="vitalinks"><a href="achats.php" class="vitalink">Achats</a> // <span id="vitaselected">Références</span> // <a href="publications.php" class="vitalink">Publications</a></h1>
