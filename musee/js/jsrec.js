b803ce84018a86e2cad88d0e519e9abb8190c7e0
Adrien Bigler <adrienbigler@MacBook-Pro-de-adrienbigler.local>
2013-11-26 17:11:25 +0100

    .gitignore is now working (I Hope)

--- musee/js/jsrec.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#up').click(function(){
-        $(this).replaceWith('<img src="'+localStorage["image1"]+'"/>')
-    })
-    $('body').html('<img src="'+localStorage["image1"]+'"/>');
-})
-
